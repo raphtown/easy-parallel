8c13803322da422d84d030e5743cb41710f445d4
Raphael Townshend <raphtown@stanford.edu>
2018-11-19 18:40:32 -0800

    Update long description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url='https://github.com/raphtown/easy-parallel',
     version='0.1.1',
     description='Parallel wrapper for easy multi-threading.',
-    long_description='Parallel wrapper for easy multi-threading.',
+    long_description=open("README.md").read(),
     author='Raphael Townshend',
     license='MIT',
     install_requires=['pathos'],
